a2f085e92406d9301512731bb0ec1c681ccd31a8
Matt-Gab <matthewe.egabriel@gmail.com>
2024-11-29 15:29:45 +0800

    working input

--- user_input_into_txt_source.py
+++ user_input_into_txt_source.py
@@ -1,11 +1,29 @@
-#- Program 1:
-#Create a program that ask user for personal information.
+#Program 1:
+# Create a program that ask user for personal information.
 # Minimum of 5 information per person, more info the better. 
 # eg. Fullname, Address, etc. Write the collected information in a txt file. 
 # It's up to you on how you'd like to format the information in the file. 
 # The program should ask user if want to input another person or exit.
 
-#- Program 2:
-#Create another program that ask user for fullname. 
-# Find the full name in the txt file (output of program1). 
-# Display the informations found in the txt file.
+# user asked for input
+# ask user to input if they want again
+# if yes then save the last input into 5 list as dictionaries and go to step 1
+# if no then save input into 4 list as dict and add the each index in the list into a list
+# put the 4 list into a txt file
+
+user_age = []
+
+while True:
+    while True:
+        try:
+            name = input('input ur name\n')
+            age = int(input('input age\n'))
+            
+            break
+        except:
+            print('error lol')
+
+    retry = input('Input another one? Y/N\n')
+    
+    if retry not in ('Y', 'y', 'yes', 'Yes', 'si'):
+        break
