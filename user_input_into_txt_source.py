8be19f38ee7a95640a4d7544e625b6a22541693b
Matt-Gab <matthewe.egabriel@gmail.com>
2024-11-29 22:34:32 +0800

    refinement of file maker

--- user_input_into_txt_source.py
+++ user_input_into_txt_source.py
@@ -9,32 +9,36 @@
 # ask user to input if they want again
 # if yes then save the last input into 5 list as dictionaries and go to step 1
 # if no then save input into 4 list as dict and add the each index in the list into a list
-# put the 4 list into a txt file
-
-user_database = []
+# put the 5 list into a txt file
 
 names = []
 ages = []
 heights = []
+countrys = []
+crushes = []
+
 
 while True:
     try:
         names.append(input('input yo name\n'))
         ages.append(int(input('input age\n')))
         heights.append(int(input('input height in cm\n')))
+        countrys.append(input('input your country\n'))
+        crushes.append(input('input your crush\n'))
         
     except:
         print('error lol')
         
-    user_database = list(zip(names, ages, heights))
-    
-    with open("./users.txt", "a") as user_iterate:
-        user_iterate.write(f'{user_database[-1]}\n')
+    user_database = list(zip(names, ages, heights, countrys, crushes))
     
     retry = input('Input another one? Y/N\n')
 
     if retry not in ('Y', 'y', 'yes', 'Yes', 'si'):
         break
-    
 
 print(user_database)
+
+with open("./users.txt", "a") as user_iterate:
+    user_iterate.write('This data is separated into\n(name, age, height, country, crush)\n')
+    for inside_list in user_database:
+        user_iterate.write('{}\n'.format(inside_list))
