6b4a412493d3ceec68c4a9b095fb467330a74291
Matt-Gab <matthewe.egabriel@gmail.com>
2024-11-30 01:24:04 +0800

    start of prog 2

--- user_read_into_txt_source.py
+++ user_read_into_txt_source.py
@@ -1,4 +1,8 @@
-#- Program 2:
-#Create another program that ask user for fullname. 
+#Program 2:
+# Create another program that ask user for fullname. 
 # Find the full name in the txt file (output of program1). 
 # Display the informations found in the txt file.
+
+searched_name = input('input the name your searching for\n')
+
+open_file = open('users.txt', 'r')
