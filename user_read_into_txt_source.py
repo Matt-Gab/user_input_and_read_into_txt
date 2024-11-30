ee645d9a6d5c1575b458b36455af0c09a0b1e6d7
Matt-Gab <matthewe.egabriel@gmail.com>
2024-11-30 22:28:09 +0800

    cool finished checkpoint but ugly

--- user_read_into_txt_source.py
+++ user_read_into_txt_source.py
@@ -5,4 +5,15 @@
 
 searched_name = input('input the name your searching for\n')
 
-open_file = open('users.txt', 'r')
+with open("./users.txt", "r") as opened_file:
+    user_data = opened_file.read()
+    changed_data_to_list = user_data.split('\n')
+    
+output = []
+
+for listed_data in changed_data_to_list:
+    if searched_name in listed_data:
+        output.append(listed_data)
+
+print('Data is ordered as Name, Age, Height(cm), Country, Crush respectively')
+print(output)
